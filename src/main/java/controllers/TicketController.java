173d81c810b9e327c2492a106924b6522d45017d
narasimha-1511 <s.narasimha.2005@gmail.com>
2024-04-30 23:52:10 +0530

    Finished

---
+++ src/main/java/controllers/TicketController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import dtos.IssueTicketRequestDto;
+import dtos.IssueTicketResponseDto;
+
+public class TicketController {
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto){
+        return null;
+    }
+}
